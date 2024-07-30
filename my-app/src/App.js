dc34898b7e017053e8371d1c8f5d2412317e303b
rezvukhinaanna <113692964+rezvukhinaanna@users.noreply.github.com>
2024-07-30 11:32:00 +0300

    Код без JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,23 +4,16 @@ import { createElement } from 'react';
 
 export function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{ /*декларативный*/}
-				<div>{(new Date().getFullYear())}</div>
-			</header>
-		</div>
+		createElement('div', {className: "App"},
+			createElement('header', {className: "App-header"},
+				createElement('img', {src: logo, className: 'App-logo', alt: 'logo'}),
+				createElement('p', null, 'Edit ',
+					createElement('code', null, 'src/App.js'),
+					'and save to reload.'
+				),
+				createElement('a', {className: "App-link", href: "https://reactjs.org", target:"_blank", rel:"noopener noreferrer"}, 'Learn React'),
+				createElement('div', null, (new Date().getFullYear()))
+			)
+		)
 	);
 }
